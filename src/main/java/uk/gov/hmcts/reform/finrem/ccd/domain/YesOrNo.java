f0fedb168c4cd0a461fd785c14fa49576a9ef0e3
EllisD-B <ellis.domengerboyce@hmcts.net>
2022-06-24 13:24:57 +0100

    add string value to yesOrNo

--- src/main/java/uk/gov/hmcts/reform/finrem/ccd/domain/YesOrNo.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/ccd/domain/YesOrNo.java
@@ -1,14 +1,32 @@
 package uk.gov.hmcts.reform.finrem.ccd.domain;
 
+import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.RequiredArgsConstructor;
 
 import static java.util.Objects.isNull;
 
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@RequiredArgsConstructor
 public enum YesOrNo {
-    YES,
-    NO;
+    YES("Yes"),
+    NO("No");
+
+    private final String yesOrNo;
+
+    @JsonValue
+    public String getYesOrNo() {
+        return yesOrNo;
+    }
+
+    public boolean isYes() {
+        return yesOrNo.equalsIgnoreCase("Yes");
+    }
+
+    public boolean isNoOrNull() {
+        return isNull(yesOrNo) || yesOrNo.equalsIgnoreCase("No");
+    }
 
     public static boolean isYes(YesOrNo yesOrNo) {
         return YES.equals(yesOrNo);
@@ -25,4 +43,5 @@ public enum YesOrNo {
     public static boolean isNoOrNull(String yesOrNo) {
         return isNull(yesOrNo) || NO.toString().equalsIgnoreCase(yesOrNo);
     }
+
 }
