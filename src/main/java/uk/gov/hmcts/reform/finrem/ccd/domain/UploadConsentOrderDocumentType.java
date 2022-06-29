6aec79e52724cbf237b0a599e8b31bd6eadc480d
EllisD-B <ellis.domengerboyce@hmcts.net>
2022-06-29 18:05:24 +0100

    add property values

--- src/main/java/uk/gov/hmcts/reform/finrem/ccd/domain/UploadConsentOrderDocumentType.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/ccd/domain/UploadConsentOrderDocumentType.java
@@ -1,9 +1,36 @@
 package uk.gov.hmcts.reform.finrem.ccd.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
 
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@RequiredArgsConstructor
 public enum UploadConsentOrderDocumentType {
-    CONSENT_ORDER, COVER_ORDER, P1, P2, PPF, PPF1, PPF2, OTHER
+    CONSENT_ORDER("consentOrder"),
+    COVER_ORDER("coverOrder"),
+    P1("P1"),
+    P2("P2"),
+    PPF("PPF"),
+    PPF1("PPF1"),
+    PPF2("PPF2"),
+    OTHER("Other");
+
+    private final String value;
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
+
+    @JsonCreator
+    public static UploadConsentOrderDocumentType forValue(String value) {
+        return Arrays.stream(UploadConsentOrderDocumentType.values())
+            .filter(option -> value.equalsIgnoreCase(option.getValue()))
+            .findFirst().orElseThrow(IllegalArgumentException::new);
+    }
 }
