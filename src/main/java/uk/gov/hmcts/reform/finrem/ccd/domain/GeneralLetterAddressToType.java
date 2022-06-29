740137dcf51eab5e13bbda7b68de290eb74536bd
EllisD-B <ellis.domengerboyce@hmcts.net>
2022-06-29 13:12:27 +0100

    update general letter enum

--- src/main/java/uk/gov/hmcts/reform/finrem/ccd/domain/GeneralLetterAddressToType.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/ccd/domain/GeneralLetterAddressToType.java
@@ -1,12 +1,32 @@
 package uk.gov.hmcts.reform.finrem.ccd.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
 
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@RequiredArgsConstructor
 public enum GeneralLetterAddressToType {
-    APPLICANT_SOLICITOR,
-    RESPONDENT_SOLICITOR,
-    RESPONDENT,
-    OTHER
+    APPLICANT_SOLICITOR("applicantSolicitor"),
+    RESPONDENT_SOLICITOR("respondentSolicitor"),
+    RESPONDENT("respondent"),
+    OTHER("other");
+
+    private final String value;
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
+
+    @JsonCreator
+    public static GeneralLetterAddressToType forValue(String value) {
+        return Arrays.stream(GeneralLetterAddressToType.values())
+            .filter(option -> value.equalsIgnoreCase(option.getValue()))
+            .findFirst().orElseThrow(IllegalArgumentException::new);
+    }
 }
