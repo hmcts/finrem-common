5b76da7d11e871dac8c15252691886a3d089dde5
EllisD-B <ellis.domengerboyce@hmcts.net>
2022-07-01 12:21:13 +0100

    update General Order pojo

--- src/main/java/uk/gov/hmcts/reform/finrem/ccd/domain/GeneralOrder.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/ccd/domain/GeneralOrder.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.finrem.ccd.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
@@ -18,13 +19,19 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 public class GeneralOrder {
-
+    @JsonProperty("generalOrder_addressTo")
     private String generalOrderAddressTo;
+    @JsonProperty("generalOrder_order")
     private String generalOrderOrder;
+    @JsonProperty("generalOrder_documentUpload")
     private Document generalOrderDocumentUpload;
+    @JsonProperty("generalOrder_judgeList")
     private JudgeType generalOrderJudgeType;
+    @JsonProperty("generalOrder_judgeName")
     private String generalOrderJudgeName;
+    @JsonProperty("generalOrder_dateOfOrder")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate generalOrderDateOfOrder;
+    @JsonProperty("generalOrder_comments")
     private String generalOrderComments;
 }
