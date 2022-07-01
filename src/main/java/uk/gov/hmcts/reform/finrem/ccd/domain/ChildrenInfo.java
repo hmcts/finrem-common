2cdf71bdbf9838c512988a7a4795e6df0008d97b
EllisD-B <ellis.domengerboyce@hmcts.net>
2022-07-01 16:38:29 +0100

    checkstyle

--- src/main/java/uk/gov/hmcts/reform/finrem/ccd/domain/ChildrenInfo.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/ccd/domain/ChildrenInfo.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
