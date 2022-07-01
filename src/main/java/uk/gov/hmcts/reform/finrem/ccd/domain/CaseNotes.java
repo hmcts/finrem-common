c7024057b2e3aa8eb6595a408f40612699ffbb08
EllisD-B <ellis.domengerboyce@hmcts.net>
2022-07-01 16:13:29 +0100

    case notes no args construct

--- src/main/java/uk/gov/hmcts/reform/finrem/ccd/domain/CaseNotes.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/ccd/domain/CaseNotes.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
@@ -12,6 +13,7 @@ import java.time.LocalDate;
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class CaseNotes {
     private String caseNoteAuthor;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
