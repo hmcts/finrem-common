73c61fd4c445f06c4d342a9c87408639876ad991
EllisD-B <ellis.domengerboyce@hmcts.net>
2022-07-11 11:24:45 +0100

    casedetails and callbackRequest builder annotations

--- src/main/java/uk/gov/hmcts/reform/finrem/ccd/domain/FinremCaseDetails.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/ccd/domain/FinremCaseDetails.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.reform.ccd.client.model.Classification;
@@ -16,6 +17,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Builder
 public class FinremCaseDetails {
 
     private long id;
