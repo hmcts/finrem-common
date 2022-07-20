ddac7455638f2a05a4a600bcbded0b3943aa5131
EllisD-B <ellis.domengerboyce@hmcts.net>
2022-07-20 11:58:51 +0100

    address json props

--- src/main/java/uk/gov/hmcts/reform/finrem/ccd/domain/Address.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/ccd/domain/Address.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.finrem.ccd.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,11 +14,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Address {
 
+    @JsonProperty("AddressLine1")
     private String addressLine1;
+    @JsonProperty("AddressLine2")
     private String addressLine2;
+    @JsonProperty("AddressLine3")
     private String addressLine3;
+    @JsonProperty("PostTown")
     private String postTown;
+    @JsonProperty("County")
     private String county;
+    @JsonProperty("PostCode")
     private String postCode;
+    @JsonProperty("Country")
     private String country;
 }
